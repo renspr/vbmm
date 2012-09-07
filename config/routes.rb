b2eeb0477b98646e00ca983ef14d267622777ed4
René Sprotte <rs@metaminded.com>
2012-09-07 19:54:17 +0200

    try to fix some routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ DiavelforumMemberMap::Application.routes.draw do
   resource  :member,   only: [:edit, :update]
   delete    :member,   to: 'members#destroy'
 
-  get '/members', to: redirect('/')
-
   resources :sessions, only: [:new, :create]
   delete    :session,  to: 'sessions#destroy'
 
+  get '/members', to: 'application#map' # hack for old route that causes problems.
   root to: 'application#map'
 
 end
