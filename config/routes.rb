f2a96aa34b31301e226e483d31d1f948c14a05b2
René Sprotte <rs@metaminded.com>
2012-09-07 15:59:50 +0200

    Fix old /members route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ DiavelforumMemberMap::Application.routes.draw do
   resource  :member,   only: [:edit, :update]
   delete    :member,   to: 'members#destroy'
 
+  get '/members', to: redirect('/')
+
   resources :sessions, only: [:new, :create]
   delete    :session,  to: 'sessions#destroy'
 
